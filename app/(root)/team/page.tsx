f545e0101ae5798046641d6045d7bd45eb6abae3
Daniel G <daniel.gao6205@gmail.com>
2024-12-14 11:56:38 -0500

    pages in team and use

---
+++ app/(root)/team/page.tsx
@@ -0,0 +1,7 @@
+export default function TeamPage() {
+    return (
+      <div>
+        <h1>About the Team</h1>
+      </div>
+    );
+  }
---
+++ app/(root)/use/page.tsx
@@ -0,0 +1,8 @@
+export default function TeamPage() {
+    return (
+      <div>
+        <h1>Use the website</h1>
+      </div>
+    );
+  }
+  
