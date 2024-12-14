8b16a8b949dcd7c8123f5c0759fb0a666cf6c31a
Daniel G <daniel.gao6205@gmail.com>
2024-12-14 11:38:10 -0500

    added firebase auth

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,3 @@
-import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
-
 export default function Layout({children} : Readonly<{children: React.ReactNode}>){
     return(
         <>
